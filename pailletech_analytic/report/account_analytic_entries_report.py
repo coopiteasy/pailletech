b91460c08df6752d33ecb163e3a584d845360fa4
houssine <houssine.bakkali@gmail.com>
2016-04-12 11:59:56 +0200

    [ADD] module to extend analytic function. this module add parent_id to analytic entries sql view to allow to filter on it in the reporting view

---
+++ pailletech_analytic/report/account_analytic_entries_report.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models, _
+from openerp import tools
+
+class AnalyticEntriesReport(models.Model):
+    _inherit = "analytic.entries.report"
+    
+    parent_id = fields.Many2one('account.analytic.account', string='Parent Analytic Account')
+    
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'analytic_entries_report')
+        cr.execute("""
+            create or replace view analytic_entries_report as (
+                 select
+                     min(a.id) as id,
+                     count(distinct a.id) as nbr,
+                     a.date as date,
+                     a.user_id as user_id,
+                     a.name as name,
+                     analytic.partner_id as partner_id,
+                     analytic.parent_id as parent_id,
+                     a.company_id as company_id,
+                     a.currency_id as currency_id,
+                     a.account_id as account_id,
+                     a.general_account_id as general_account_id,
+                     a.journal_id as journal_id,
+                     a.move_id as move_id,
+                     a.product_id as product_id,
+                     a.product_uom_id as product_uom_id,
+                     sum(a.amount) as amount,
+                     sum(a.unit_amount) as unit_amount
+                 from
+                     account_analytic_line a, account_analytic_account analytic
+                 where analytic.id = a.account_id
+                 group by
+                     a.date, a.user_id,a.name,analytic.partner_id,analytic.parent_id,a.company_id,a.currency_id,
+                     a.account_id,a.general_account_id,a.journal_id,
+                     a.move_id,a.product_id,a.product_uom_id
+            )
+        """)
