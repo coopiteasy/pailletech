0d3f838babc14f6377e407beda28eb510e13e5a1
houssine <houssine.bakkali@gmail.com>
2017-08-24 19:08:52 +0200

    [IMP] add parent analytic account on analytic line

---
+++ pailletech_analytic/models/account_analytic.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#  2015-2017 Coop IT Easy (<http://www.coopiteasy.be>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+    
+    parent_analytic_account = fields.Many2one(related='account_id.parent_id', string="Parent Analytic Account")
