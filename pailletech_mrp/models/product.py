192f833671e221ffcdaac9f787c2a210a46ec9ee
houssine <houssine.bakkali@gmail.com>
2018-01-10 11:49:18 +0100

    [IMP] when a product is being deactivated we check  if the product is not in a bom line and raise an error if so.

--- pailletech_mrp/models/product.py
+++ pailletech_mrp/models/product.py
@@ -16,6 +16,17 @@ class ProductTemplate(models.Model):
     uom_reference = fields.Many2one('product.uom', compute="_compute_reference_uom", string="Reference UoM", help="Reference Unit of Measure for the UoM category of this product")
     uom_category = fields.Many2one(related='uom_id.category_id',string="UoM Category", readonly=True, help="Category of this product")
     
+    @api.multi
+    def write(self, vals):
+        if vals.has_key('active'):
+            if vals.get('active') == False:
+                product_id = self.product_variant_ids.id
+                lines = self.env['mrp.bom.line'].search([('product_id','=',product_id)])
+                if len(lines) > 0:
+                    raise except_orm(_('Error!'),
+                    _('The product still referenced in a bill of material line. Please remove the line containing the product before to deactivate it'))
+        return super(ProductTemplate,self).write(vals)
+            
 class ProductUOMCategory(models.Model):
     _inherit = 'product.uom.categ'
     _order = "name"
